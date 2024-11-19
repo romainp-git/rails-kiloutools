114f749ca73e26ffb4d870e73054cca6b95a4b33
romain portier <romain.portier@wanadoo.fr>
2024-11-19 12:13:24 +0100

    bugs fix

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,8 @@
 import { Application } from "@hotwired/stimulus"
-import DatepickerController from "./datepicker_controller";
 
 const application = Application.start()
 
 application.debug = false
 window.Stimulus   = application
-application.register("datepicker", DatepickerController);
 
 export { application }
