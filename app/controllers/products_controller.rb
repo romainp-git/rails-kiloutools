94c28fbb0b3907bd82133fa0de16d35ead2fd88a
PYM5 <pymsoft5@gmail.com>
2024-11-19 10:58:38 +0100

    CRUD Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  # INDEX + SHOW ========================
   def index
     @products = filter_products(params[:search])
   end
 
   def show
-    @product = Product.find(params[:id])
+    @fTitle = "View #{@product.name} (#{@product.id})"
   end
 
   def search
@@ -12,7 +15,84 @@ class ProductsController < ApplicationController
     render :index
   end
 
+  # NEW + CREATE ========================
+  def new
+    @product = Product.new
+    @fTitle = "Create a new product"
+  end
+
+  def create
+    @product = Product.new(product_params)
+    begin
+      if @product.save
+        flash[:notice] = "Product was successfully created"
+        redirect_to @product
+      else
+        flash[:alert] = "Failed to create product"
+        render :new
+      end
+    rescue => e
+      log_error(e)
+      flash[:alert] = "Failed to create product"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  # EDIT + UPDATE ========================
+  def edit
+    @fTitle = "Modify #{@product.name} (#{@product.id})"
+  end
+
+  def update
+    begin
+      if @product.update(product_params)
+        flash[:notice] = "Product was successfully updated"
+        redirect_to @product
+      else
+        flash[:alert] = "Failed to update product"
+        render :edit
+      end
+    rescue => e
+      log_error(e)
+      flash[:alert] = "Failed to update product"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  # DESTROY ========================
+  def destroy
+    begin
+      if @product.destroy
+        flash[:notice] = "Product was successfully destroyed"
+        redirect_to products_path
+      else
+        flash[:alert] = "Failed to delete product"
+        redirect_to product_path(@product)
+      end
+    rescue => e
+      log_error(e)
+      flash[:alert] = "Failed to delete product"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  # =================================
   private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :state, :model, :photo_url, :price)
+  end
+
+  def set_product
+    # Si l'id demandé n'existe pas alors retour sur index et affichage message alert
+    begin
+      @product = Product.find(params[:id])
+    rescue => e
+      log_error(e)
+      flash[:alert] = "Product not found"
+      redirect_to products_path
+    end
+  end
 
   def filter_products(search_params)
     products = Product.all
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,22 @@ class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :category
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
-  
+
   has_many :bookings
   has_many :customers
 
+  STATES = ["New", "Like new", "Good", "Fair", "Poor"]
+
+  validates :name, :description, :state, :model, :photo_url, :price, presence: true
+  validates :state, inclusion: { in: STATES, message: "%{value} isn't a valid state" }
+  validates :photo_url, format: { with: /\A(http|https):\/\/[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)+(:\d+)?(\/[^\s]*)?\z/i, message: "is invalid" }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_user_id?
+
+  def can_be_destroyed?
+    bookings.empty?
+  end
 
   def address
     owner.address
