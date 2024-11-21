f2765bd584427e51505dc3b194764f5748fb27b9
samuel willem <samuelwillem@hotmail.fr>
2024-11-21 11:53:19 +0100

    change booking form and controller

--- app/javascript/controllers/status_controller.js
+++ app/javascript/controllers/status_controller.js
@@ -4,21 +4,17 @@ export default class extends Controller {
   static targets = ["select"];
 
   updateStatus(event) {
-    const bookingId = event.target.dataset.bookingId; // Récupère l'ID de la réservation
-    const newStatus = event.target.value; // Récupère le nouveau statut
+    const bookingId = event.target.dataset.bookingId;
+    const newStatus = event.target.value;
 
-    // Demander confirmation avant de mettre à jour
     if (!confirm('Êtes-vous sûr de vouloir changer le statut de cette réservation ?')) {
-      // Si l'utilisateur annule, on arrête l'exécution
+
       return;
     }
-
-    // Envoi de la requête PATCH avec le token CSRF
     fetch(`/bookings/${bookingId}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('[name="csrf-token"]').content // Assurez-vous d'inclure le token CSRF
       },
       body: JSON.stringify({
         booking: {
