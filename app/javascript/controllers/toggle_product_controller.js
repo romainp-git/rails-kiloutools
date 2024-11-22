4d168753a8e0e7ed6285f24845d77beea6a3208d
samuel willem <samuelwillem@hotmail.fr>
2024-11-22 11:35:05 +0100

    fix missing picture

--- app/javascript/controllers/toggle_product_controller.js
+++ app/javascript/controllers/toggle_product_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["checkbox"]
+  static targets = ["checkbox", "badge"]
 
   connect() {
-
     console.log("Le contrôleur est connecté");
   }
 
@@ -12,14 +11,35 @@ export default class extends Controller {
     const productId = this.checkboxTarget.dataset.productId;
     const isActive = this.checkboxTarget.checked;
 
-    fetch(`/products/${productId}/update_active`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ active: isActive })
-    })
-    .then(response => response.json())
+    this.updateBadge(isActive);
 
+    fetch(`/products/${productId}/update_active`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ active: isActive }),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .catch(error => console.error("Error updating product status:", error));
+  }
+
+  updateBadge(isActive) {
+    const badge = this.badgeTarget;
+
+    if (isActive) {
+      badge.classList.remove("bg-danger");
+      badge.classList.add("bg-success");
+      badge.textContent = "En ligne";
+    } else {
+      badge.classList.remove("bg-success");
+      badge.classList.add("bg-danger");
+      badge.textContent = "Hors ligne";
+    }
   }
 }
